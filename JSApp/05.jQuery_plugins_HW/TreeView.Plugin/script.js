f7c53daa4616219256ed5ff0f05f26b8e54d2ae2
Bunny303 <303bunnies@gmail.com>
2014-01-12 11:54:24 +0200

    Add homeworks

---
+++ JSApp/05.jQuery_plugins_HW/TreeView.Plugin/script.js
@@ -0,0 +1,17 @@
+﻿(function ($) {
+    $.fn.treeView = function () {
+        var a = this.find("a");
+        a.on("click", function () {
+            var ul = $(this).next("ul");
+            if (ul.hasClass("collapsed")) {
+                ul.removeClass("collapsed").show();
+            }
+            else {
+                ul.addClass("collapsed").hide();
+            }
+        });
+        return this;
+    }
+}(jQuery));
+
+// to see .toggle()
